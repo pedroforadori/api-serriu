eb1be35fd976da7ff8903d02b270c4bbccf9d4af
unknown <pedro.foradori@dieboldnixdorf.com>
2020-10-07 12:10:08 -0300

    first commit

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+    async register({ request }){
+        const data = request.only(['username', 'email', 'password'])
+        
+        const user = await User.create(data)
+
+        return user
+    }
+
+    async index(){
+        const user = await User.all()
+
+        return user
+    }
+}
+
+
+module.exports = UserController
